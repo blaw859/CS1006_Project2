5f7bfc960158394b3657fb488e509734b0410188
Anders <aindreboe@gmail.com>
2018-03-03 13:39:00 +0000

    New EnergyMatrix method
    I have added the method to create the energy matrix into the Image class

--- CS1006P2/src/Image.java
+++ CS1006P2/src/Image.java
@@ -7,13 +7,12 @@ public class Image {
     private int width;
     private int height;
     private BufferedImage bufferedImage;
+    private double[][] energymatrix;
 
     //Constructs an image object
     public Image(File imageFile) throws IOException{
         bufferedImage = ImageIO.read(imageFile);
-
-
-
+        energymatrix = energyMatrix();
     }
 
     //Returns an array with the RGB in that order of a pixel (x,y)
@@ -28,4 +27,50 @@ public class Image {
         rgbArray[2] = (argbColour & 0x000000ff); //Blue
         return rgbArray;
     }
+
+    //Returns an energy matrix as a 2D array of double values
+    private double[][] energyMatrix() {
+        double[][] energyArray;
+        energyArray = new double[bufferedImage.getNumXTiles()][bufferedImage.getNumYTiles()];
+        double energyX;
+        double energyY;
+
+        for (int x = 0; x < bufferedImage.getNumXTiles() - 1; x++) {
+            for (int y = 0; y < bufferedImage.getNumYTiles() - 1; y++) {
+                int l = x - 1;
+                int r = x + 1;
+                int a = y + 1;
+                int b = y - 1;
+
+                //These statements deal with the edges of the array
+                if(x == 0) l = bufferedImage.getNumXTiles() - 1;
+                if(y == 0) a = bufferedImage.getNumYTiles() - 1;
+                if(x == bufferedImage.getNumXTiles()) r = 0;
+                if(y == bufferedImage.getNumYTiles()) b = 0;
+
+                int[] localRGBleft = getPixelColours(l,y);
+                int[] localRGBright = getPixelColours(r,y);
+                int[] localRGBabove = getPixelColours(x,a);
+                int[] localRGBbelow = getPixelColours(x,b);
+
+                //The difference between the left and right pixel are squared and added together
+                //This value is then added to the difference between the above and below pixel and averaged
+                energyX = (
+                        (localRGBright[0] - localRGBleft[0])*(localRGBright[0] - localRGBleft[0]) +
+                        (localRGBright[2] - localRGBleft[2])*(localRGBright[2] - localRGBleft[2]) +
+                        (localRGBright[1] - localRGBleft[1])*(localRGBright[1] - localRGBleft[1]));
+                energyY = (
+                        (localRGBabove[0] - localRGBbelow[0])*(localRGBabove[0] - localRGBbelow[0]) +
+                        (localRGBabove[2] - localRGBbelow[2])*(localRGBabove[2] - localRGBbelow[2]) +
+                        (localRGBabove[1] - localRGBbelow[1])*(localRGBabove[1] - localRGBbelow[1]));
+                energyArray[x][y] = Math.sqrt(energyX + energyY);
+            }
+        }
+        return energyArray;
+    }
+
+    public double[][] getEnergymatrix() {
+        return energymatrix;
+    }
+
 }
